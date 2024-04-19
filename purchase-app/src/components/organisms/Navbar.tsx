446281c1321c5dee3f4521aba108a7801860fc9d
Pablo Garay <elxebec@gmail.com>
2024-04-19 12:16:00 -0300

    Delete comments

--- purchase-app/src/components/organisms/Navbar.tsx
+++ purchase-app/src/components/organisms/Navbar.tsx
@@ -16,17 +16,6 @@ const Navbar = () => {
             </button>
         </Link>
         <TextInput onChange={handleChangeInput} />
-        {/* <form action="/pages/search" method="GET" className="flex-grow flex items-center justify-center">
-          <input
-            type="text"
-            placeholder="Search products..."
-            className="border border-gray-300 p-2 rounded-full w-full max-w-md"
-            name="q"
-          />
-          <button type="submit" className="bg-blue-500 text-white px-4 py-1 rounded-full ml-2">
-            Search
-          </button>
-        </form> */}
         <div className="flex items-center space-x-4">
           <Link href="/pages/account/login" passHref>
             <button className="btn btn-success">Account</button>
