1f4958b0641e0ad0e664b17354e006921ef1f156
Pablo Garay <elxebec@gmail.com>
2024-04-19 12:15:41 -0300

    Create text input

---
+++ purchase-app/src/components/molecules/TextInput.tsx
@@ -0,0 +1,49 @@
+"use client"
+import { useState } from "react";
+
+interface ITextInputProps {
+  onChange: (value: string) => void,
+}
+
+const TextInput = ({ onChange }: ITextInputProps) => {
+  const [inputState, setInputState] = useState<string>("");
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value: string = e.target.value
+    setInputState(value.toLocaleLowerCase());
+  }
+
+  const handlePushEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSubmit()
+    }
+  }
+
+  const handleSubmit = () => {
+    setInputState("")
+    onChange(inputState)
+  };
+
+  return ( 
+    <div className="flex-grow flex items-center justify-center">
+      <input
+        className="border border-gray-300 p-2 rounded-full w-full max-w-md"
+        onChange={handleChangeInput}
+        onKeyDown={handlePushEnter}
+        type="text"
+        placeholder="Search product"
+        name="text input"
+        required
+      />
+      <button
+        className="bg-black-btn text-white px-4 py-1 rounded-full ml-2"
+        onClick={handleSubmit}
+        disabled={!inputState}
+      >
+        Search
+      </button>
+    </div>
+  );
+}
+
+export default TextInput;
