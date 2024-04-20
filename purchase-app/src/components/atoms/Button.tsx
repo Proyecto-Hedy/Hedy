21e8c7f3e4ee7dbc98d7cfc16321219de47ed952
Pablo Garay <elxebec@gmail.com>
2024-04-20 17:54:33 -0300

    Improve custom button

--- purchase-app/src/components/atoms/Button.tsx
+++ purchase-app/src/components/atoms/Button.tsx
@@ -1,13 +1,19 @@
 interface IButtonProps {
   name: string;
-  onClick?: () => void
+  onClick?: () => void;
+  className?: string;
+  disabled?: boolean;
 }
 
-const Button = ({ name, onClick }: IButtonProps) => {
+const Button = ({ name, onClick, className, disabled=false }: IButtonProps) => {
   return (
     <button
       type="submit"
-      className="w-full mb-8 mt-8 bg-black-btn hover:bg-black-hover hover:text-white text-xl text-gray-bg-light p-4 rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
+      disabled={disabled}
+      className={`w-full mb-8 mt-8 p-4 rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]
+      ${disabled ? "bg-gray-bg-light text-white hover:bg-gray-bg-light cursor-not-allowed" : ""}
+      ${className ? className : "bg-black-btn hover:bg-black-hover hover:text-white text-xl font-medium text-gray-bg-light"}
+      `}
       onClick={onClick}
     >
       {name}
