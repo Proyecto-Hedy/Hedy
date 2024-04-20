204a3de0c956828452005e630ba599ebcf25cc16
Nicolas Nievas <nicoonievas7@gmail.com>
2024-04-19 23:39:15 -0300

    styless

--- purchase-app/src/modules/products/components/product-preview.tsx
+++ purchase-app/src/modules/products/components/product-preview.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IProductData } from "@/interfaces/data.interfaces";
 import Image from "next/image";
+import styles from "../template/product-template.module.css";
 
 interface ProductPreviewProps {
   product: IProductData;
@@ -8,21 +9,21 @@ interface ProductPreviewProps {
 
 const ProductPreview: React.FC<ProductPreviewProps> = ({ product }) => {
   return (
-    <div className="max-w-xs rounded-lg overflow-hidden shadow-md relative">
-      <Image
-        src={product.thumbnail}
-        alt={product.title}
-        width={300}
-        height={300}
-      />
-      <div className="px-6 py-4 absolute bottom-0 left-0 right-0 bg-white bg-opacity-75">
-        <div className="font-bold text-xl mb-2">{product.title}</div>
-        <p className="text-gray-700 text-base">${product.price}</p>
+    <div className={`${styles["product-preview"]} max-w-xs rounded-lg overflow-hidden shadow-md relative`}>
+      <div className={`${styles["image-wrapper"]}`}>
+        <Image
+          src={product.thumbnail}
+          alt={product.title}
+          width={300}
+          height={300}
+        />
+      </div>
+      <div className="px-4 py-2">
+        <div className="font-bold text-lg mb-1">{product.title}</div>
+        <p className="text-gray-700">${product.price}</p>
       </div>
     </div>
   );
 };
 
 export default ProductPreview;
-
-
