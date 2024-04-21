0034af2eb40fae17339415099192ce0feaae100c
Pablo Garay <elxebec@gmail.com>
2024-04-21 18:41:07 -0300

    Fix style issue

--- purchase-app/src/app/pages/account/page.tsx
+++ purchase-app/src/app/pages/account/page.tsx
@@ -8,7 +8,7 @@ export default function Account() {
     const [currentView, setCurrentView] = useState(LOGIN_VIEW.SIGN_IN)
 
     return (
-        <div className="min-h-screen bg-white flex items-center justify-center">
+        <div className="min-h-screen flex items-center justify-center">
             {currentView === LOGIN_VIEW.SIGN_IN ? (
                 <Login setCurrentView={setCurrentView} />
             ) : (
