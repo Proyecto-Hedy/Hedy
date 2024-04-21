237853f0b19d8b60fac76e6f3b04c156f1f82293
Pablo Garay <elxebec@gmail.com>
2024-04-21 18:43:21 -0300

    Added custom notification

--- purchase-app/src/services/auth/signIn.ts
+++ purchase-app/src/services/auth/signIn.ts
@@ -2,16 +2,21 @@ import firebase_app from "../firebase";
 import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
 import { AuthResponse } from "@/interfaces/data.interfaces";
 import { RESPONSE_STATUS } from "@/interfaces/enums";
+import ErrorHandler from "@/helper/errorHandler.helper";
 
 const auth = getAuth(firebase_app);
 
 const signIn = async (email: string, password: string): Promise<AuthResponse> => {
     try {
       const response = await signInWithEmailAndPassword(auth, email, password);
-      return { response, status: RESPONSE_STATUS.CREATED }
+      return { response, status: RESPONSE_STATUS.CREATED, message: "Sign in successfully" }
     } catch (error: any) {
       const errorMessage  = error.message
-      return { errorMessage , status: RESPONSE_STATUS.BAD_REQUEST }
+      const errorCode = error.code;
+
+      const message = ErrorHandler(errorCode, errorMessage)
+
+      return { message , status: RESPONSE_STATUS.BAD_REQUEST }
     }
 }
 
--- purchase-app/src/services/auth/signUp.ts
+++ purchase-app/src/services/auth/signUp.ts
@@ -2,16 +2,21 @@ import firebase_app from "../firebase";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { AuthResponse } from "@/interfaces/data.interfaces";
 import { RESPONSE_STATUS } from "@/interfaces/enums";
+import ErrorHandler from "@/helper/errorHandler.helper";
 
 const auth = getAuth(firebase_app);
 
 const signUp = async (email: string, password: string): Promise<AuthResponse> => {
   try {
     const response = await createUserWithEmailAndPassword(auth, email, password)
-    return { response, status: RESPONSE_STATUS.CREATED }
+    return { response, status: RESPONSE_STATUS.CREATED, message: "Member profile created successfully" }
   } catch (error: any) {
     const errorMessage  = error.message
-    return { errorMessage , status: RESPONSE_STATUS.BAD_REQUEST }
+    const errorCode = error.code;
+
+    const message = ErrorHandler(errorCode, errorMessage)
+
+    return { message , status: RESPONSE_STATUS.BAD_REQUEST }
   }
 }
 
