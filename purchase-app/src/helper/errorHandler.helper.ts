22765ab571f1bdb817925ba343aaa6e7ba020787
Pablo Garay <elxebec@gmail.com>
2024-04-21 18:42:31 -0300

    Created error handler function

---
+++ purchase-app/src/helper/errorHandler.helper.ts
@@ -0,0 +1,18 @@
+export default function ErrorHandler(errorCode: string, message: string) {
+  switch (errorCode) {
+    case "auth/weak-password":
+      return "The password is too weak."
+    case "auth/email-already-in-use":
+      return "This email address is already in use by another account."
+    case "auth/invalid-credential":
+      return "This email/password are invalid."
+    case "auth/invalid-email":
+      return "This email address is invalid."
+    case "auth/operation-not-allowed":
+      return "Email/password accounts are not enabled."
+    case "auth/user-disabled":
+      return "Email account is disabled."
+    default:
+      return message
+  }
+}
