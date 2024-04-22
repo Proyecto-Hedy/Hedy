ef1c7548d4959aeaf034b33dab999de61a6abde7
Pablo Garay <elxebec@gmail.com>
2024-04-21 23:08:51 -0300

    Added google login

--- purchase-app/src/app/pages/account/login/Login.tsx
+++ purchase-app/src/app/pages/account/login/Login.tsx
@@ -8,7 +8,7 @@ import { useDataContext } from "@/context/data.context";
 import { navigate } from "@/services/actions";
 import { toast } from "react-toastify";
 import signInWithGoogle from "@/services/auth/signInWithGoogle";
-import Image from "next/image";
+import GoogleImage from "@/components/atoms/GoogleImage";
 
 interface ILoginProps {
   setCurrentView: (view: LOGIN_VIEW) => void;
@@ -31,7 +31,7 @@ const Login = ({ setCurrentView }: ILoginProps) => {
       toast.error(message, { toastId: "fail" })
     }
     else {
-      setUser(response!.email)
+      setUser(response!)
       toast.success(message, { toastId: "success" })
       navigate("/")
     }
@@ -47,7 +47,7 @@ const Login = ({ setCurrentView }: ILoginProps) => {
       toast.error(message, { toastId: "fail" })
     }
     else {
-      setUser(response!.email)
+      setUser(response!)
       toast.success(message, { toastId: "success" })
       navigate("/")
     }
@@ -74,13 +74,7 @@ const Login = ({ setCurrentView }: ILoginProps) => {
           </h2>
           <p className="text-base">Sign in to access an enhanced shopping experience.</p>
           <form className="relative" onSubmit={handleSubmitWithGoogle}>
-            <Image 
-            className="absolute bottom-[40%] left-[5%]"
-              src={`data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 48 48'%3E%3Cdefs%3E%3Cpath id='a' d='M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z'/%3E%3C/defs%3E%3CclipPath id='b'%3E%3Cuse xlink:href='%23a' overflow='visible'/%3E%3C/clipPath%3E%3Cpath clip-path='url(%23b)' fill='%23FBBC05' d='M0 37V11l17 13z'/%3E%3Cpath clip-path='url(%23b)' fill='%23EA4335' d='M0 11l17 13 7-6.1L48 14V0H0z'/%3E%3Cpath clip-path='url(%23b)' fill='%2334A853' d='M0 37l30-23 7.9 1L48 0v48H0z'/%3E%3Cpath clip-path='url(%23b)' fill='%234285F4' d='M48 48L17 24l-4-3 35-10z'/%3E%3C/svg%3E`}
-              width={40}
-              height={40}
-              alt="google"
-            />
+            <GoogleImage classname="absolute bottom-[40%] left-[5%]" width={40} height={40} />
             <Button name="Continue with google" className="hover:font-bold" />
           </form>
           <div className="w-full flex items-center before:content-[''] before:border-b-2 before:h-1.5 before:flex-1 after:border-b-2 after:h-1.5 after:flex-1">
