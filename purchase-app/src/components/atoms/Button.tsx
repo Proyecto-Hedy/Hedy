e74e82f8fc8b29b4c39a92b3bedd6726d930e7d8
Pablo Garay <elxebec@gmail.com>
2024-04-22 18:20:05 -0300

    Improve styles

--- purchase-app/src/components/atoms/Button.tsx
+++ purchase-app/src/components/atoms/Button.tsx
@@ -13,9 +13,9 @@ const Button = ({ name, onClick, className, isDisabled=false, isLoading }: IButt
     <button
       type="submit"
       disabled={isDisabled}
-      className={`w-full h-[100px] mb-8 mt-8 p-4 rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]
+      className={`w-full h-[100px] p-4 rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]
       ${isDisabled ? "bg-gray-bg-light text-white hover:bg-gray-bg-light cursor-not-allowed" : ""}
-      ${className ? className : "bg-black-btn hover:bg-black-hover hover:text-white text-xl font-medium text-gray-bg-light"}
+      ${className ? className : "mb-8 mt-8 bg-black-btn hover:bg-black-hover hover:text-white text-xl font-medium text-gray-bg-light"}
       `}
       onClick={onClick}
     >
