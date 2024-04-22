c391523dfefb4628f0ec8740401fe421dc68249e
Pablo Garay <elxebec@gmail.com>
2024-04-21 23:11:47 -0300

    Implemented google account component

--- purchase-app/src/components/organisms/Navbar.tsx
+++ purchase-app/src/components/organisms/Navbar.tsx
@@ -9,6 +9,7 @@ import TextInput from '../molecules/TextInput';
 import { useDataContext } from '@/context/data.context';
 import logOut from '@/services/auth/signOut';
 import { toast } from 'react-toastify';
+import GoogleAccount from '../molecules/GoogleAccount';
 
 const Navbar = () => {
   const { user } = useDataContext()
@@ -35,18 +36,23 @@ const Navbar = () => {
           />
         </Link>
         <TextInput onChange={handleChangeInput} />
-        <div className="flex items-center w-1/6">
+        <div className="flex items-center">
           <div
             className='relative'
             onMouseOver={() => setIsHover(true)}
             onMouseLeave={() => setIsHover(false)}
           >
-            {/* className="p-4 hover:text-gray-bg-light hover:underline hover:decoration-gray-bg-light" */}
-            <Link href="/pages/account">
-              <button className="p-4 hover:text-gray-bg-light hover:underline hover:decoration-gray-bg-light">{user ? user.substring(0, user.lastIndexOf("@")) : "Account"}</button>
-            </Link>
+            {user ? 
+              <GoogleAccount user={user!} /> 
+              : (
+                <Link href="/pages/account">
+                  <button className="p-4 hover:text-gray-bg-light hover:underline hover:decoration-gray-bg-light">
+                    Account
+                  </button>
+                </Link>)
+            }
             {isHover && (
-              <div className='z-50 absolute left-[15%] flex flex-col justify-center bg-gray-bg text-sm'>
+              <div className='z-50 absolute top-[85%] border rounded-lg border-gray-bg-light left-[5%] flex flex-col justify-center bg-gray-bg text-sm'>
                 {!user ? (
                   <Link className='p-2 hover:bg-gray-bg-light hover:text-white rounded-lg' href="/pages/account">
                     Sign in
@@ -65,7 +71,7 @@ const Navbar = () => {
             }
           </div>
           <Link href="/pages/cart" passHref>
-            <button className="p-3 border rounded-lg shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">
+            <button className="p-3 border rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
             <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-shopping-cart hover:translate-y-px hover:translate-x-px"><circle cx="8" cy="21" r="1"/><circle cx="19" cy="21" r="1"/><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"/></svg>
             </button>
           </Link>
