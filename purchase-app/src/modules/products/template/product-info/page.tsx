e84b7c1aea6e4198f579b0b2ebf81ed184a84075
Nicolas Nievas <nicoonievas7@gmail.com>
2024-04-22 01:58:09 -0300

    galeria separada

--- purchase-app/src/modules/products/template/product-info/page.tsx
+++ purchase-app/src/modules/products/template/product-info/page.tsx
@@ -28,7 +28,7 @@ const ProductDetail: React.FC<ProductDetailProps> = ({ product }) => {
           <ProductTabs product={product} />
         </div>
       </div>
-      <div className="image-gallery" style={{ display: 'flex', justifyContent: 'center', gap: '10px', flexWrap: 'wrap', marginTop: '20px' }}>
+      <div className="image-gallery" style={{ display: 'flex', justifyContent: 'center', gap: '10px', flexWrap: 'wrap', marginTop: '40px', marginBottom: '40px', padding: '20px', border: '1px solid #ddd', borderRadius: '15px' }}>
         {otherImages.map((image, index) => (
           <div key={index} style={{ margin: '5px' }}>
             <Image src={image} alt={`Gallery image ${index}`} width={300} height={300} />
