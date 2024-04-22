6e6c94771c3c2640ec19b460d6c7aea105bb17fc
Nicolas Nievas <nicoonievas7@gmail.com>
2024-04-22 15:47:05 -0300

    w, h ajustados

--- purchase-app/src/modules/products/components/image-gallery/page.tsx
+++ purchase-app/src/modules/products/components/image-gallery/page.tsx
@@ -10,7 +10,7 @@ const ImageGallery: React.FC<ImageGalleryProps> = ({ images }) => {
     <div className="image-gallery" style={{ display: 'flex', justifyContent: 'center', gap: '10px', flexWrap: 'wrap', marginTop: '40px', marginBottom: '40px', padding: '20px', border: '1px solid #ddd', borderRadius: '15px' }}>
       {images.map((image, index) => (
         <div key={index} style={{ margin: '5px' }}>
-          <Image src={image} alt={`Gallery image ${index}`} width={300} height={300} objectFit="cover" />
+          <Image src={image} alt={`Gallery image ${index}`} width={250} height={250} objectFit="cover" />
         </div>
       ))}
     </div>
