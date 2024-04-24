bbcf8138fa679bc32233b92e6bd530414cdb1ce0
Nicolas Nievas <nicoonievas7@gmail.com>
2024-04-23 21:03:25 -0300

    comentarios

--- purchase-app/src/modules/cart/components/cart-alert/page.tsx
+++ purchase-app/src/modules/cart/components/cart-alert/page.tsx
@@ -5,13 +5,11 @@ import "react-toastify/dist/ReactToastify.css";
 const cartAlert = ({ product }: { product: any }) => {
   const { title } = product;
 
-  // Mostrar la notificación
   toast.success(
     <div className="flex items-center text-black">
       <p className="font-semibold mr-2">¡Agregado al carrito!</p>
       <p>{title}</p>
     </div>,
-    //href={`pages/cart}`
     {
       position: "top-right",
       autoClose: 3000,
