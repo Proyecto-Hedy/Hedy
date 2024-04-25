b1d5951bd756df032549dc21bb2da2d2512ae837
Nicolas Nievas <nicoonievas7@gmail.com>
2024-04-24 23:20:35 -0300

    obtencion de usuario

--- purchase-app/src/app/pages/profile/page.tsx
+++ purchase-app/src/app/pages/profile/page.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+"use client";
+import React, { useContext } from "react";
 import { Metadata } from "next";
+import { useDataContext } from "@/context/data.context";
 
-export const metadata: Metadata = {
-    title: "Profile",
-    description: "profile",
-    };
 
-export default function profile() {
+
+const Profile: React.FC = () => {
+    const { user} = useDataContext();
+
     return (
         <div>
-        <h1>Profile</h1>
+            <h1>Profile</h1>
+            {user ? (
+                <p>Welcome, {user.email}</p> 
+            ) : (
+                <p>Please sign in to view your profile.</p>
+            )}
         </div>
     );
-    }
+};
+
+export default Profile;
