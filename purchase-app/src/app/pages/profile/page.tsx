dc1cdfdb0882b1d5e748a0bf20656b728cc1653a
Nicolas Nievas <nicoonievas7@gmail.com>
2024-04-24 21:21:20 -0300

    redirec profile

---
+++ purchase-app/src/app/pages/profile/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Profile",
+    description: "profile",
+    };
+
+export default function profile() {
+    return (
+        <div>
+        <h1>Profile</h1>
+        </div>
+    );
+    }
