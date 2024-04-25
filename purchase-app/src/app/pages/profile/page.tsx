e8a821fe6797dec437a6ef6e5cd52ce26ae7ffbb
Nicolas Nievas <nicoonievas7@gmail.com>
2024-04-25 18:03:54 -0300

    base estilos

--- purchase-app/src/app/pages/profile/page.tsx
+++ purchase-app/src/app/pages/profile/page.tsx
@@ -1,23 +1,25 @@
 "use client";
-import React, { useContext } from "react";
+import React from "react";
 import { Metadata } from "next";
 import { useDataContext } from "@/context/data.context";
 
-
-
 const Profile: React.FC = () => {
-    const { user} = useDataContext();
+  const { user } = useDataContext();
 
-    return (
-        <div>
-            <h1>Profile</h1>
-            {user ? (
-                <p>Welcome, {user.email}</p> 
-            ) : (
-                <p>Please sign in to view your profile.</p>
-            )}
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="max-w-lg bg-white p-8 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+        <h1 className="text-2xl font-semibold mb-4">Profile</h1>
+        <div className="text-lg mb-4">
+          {user ? (
+            <p>Welcome, {user.email}</p>
+          ) : (
+            <p>Please sign in to view your profile.</p>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
