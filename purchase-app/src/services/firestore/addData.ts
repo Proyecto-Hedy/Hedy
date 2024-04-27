842948489a2c18f988cd142c857f19d0bbcc7f31
Pablo Garay <elxebec@gmail.com>
2024-04-27 20:15:45 -0300

    Created add data to firestore function

---
+++ purchase-app/src/services/firestore/addData.ts
@@ -0,0 +1,17 @@
+import { collection, addDoc } from "firebase/firestore"; 
+import firebase from "../firebase";
+import { AuthResponse, IProductData } from "@/interfaces/data.interfaces";
+import { RESPONSE_STATUS } from "@/interfaces/enums";
+
+const db = firebase.database
+
+const addData = async (body: IProductData[], email: string): Promise<AuthResponse> => {
+  try {
+    await Promise.all(body.map((product: IProductData) => addDoc(collection(db, "order"), body)))
+    return { status: RESPONSE_STATUS.CREATED, message: "Record successfully inserted in the database" }
+  } catch (error: any) {
+    return { message: error.message , status: RESPONSE_STATUS.BAD_REQUEST }
+  }
+}
+
+export default addData
