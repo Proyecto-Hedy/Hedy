fe4ac32cf671eb54f163202712a0fac8c631dd21
Pablo Garay <elxebec@gmail.com>
2024-04-28 17:26:57 -0300

    Created delivery card component

--- purchase-app/src/components/molecules/DeliveryCard.tsx
+++ purchase-app/src/components/molecules/DeliveryCard.tsx
@@ -1,15 +1,15 @@
 interface DeliveryCardProps {
   title: string
   price: number
-  onClick: (p: number) => void
+  onClick: (p: number, t: string) => void
 }
 
 const DeliveryCard = ({ title, price, onClick }: DeliveryCardProps) => {
   return ( 
     <div>
-      <div className="flex items-center justify-between text-small-regular cursor-pointer py-4 border rounded-rounded px-8 mb-2 hover:shadow-borders-interactive-with-active">
+      <div className="flex items-center justify-between text-small-regular cursor-pointer py-4 border rounded-rounded px-8 mb-2 hover:border-blue">
         <div className="flex items-center gap-x-4">
-          <input type="checkbox" onClick={() => onClick(price)} />
+          <input type="checkbox" onClick={() => onClick(price, title)} />
           <span>{title}</span>
         </div>
         <span>$ {price}</span>
