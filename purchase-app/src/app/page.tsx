33f400f37aa6fbabb9eb2dc93b28c89a3bdce893
Pablo Garay <elxebec@gmail.com>
2024-04-28 17:27:49 -0300

    Added filter

--- purchase-app/src/app/page.tsx
+++ purchase-app/src/app/page.tsx
@@ -1,17 +1,13 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Container from "@/components/molecules/Container";
 import ProductsTemplate from "@/modules/products/template/product-template";
 import Pagination from "@/modules/pagination/page";
 import { useApi } from "@/hooks/useApi";
-import { IDataResponse, IProductData } from "@/interfaces/data.interfaces";
+import { IDataResponse } from "@/interfaces/data.interfaces";
 import LoadingSpinner from "@/components/atoms/LoadingSpinner";
-import { useDataContext } from "@/context/data.context";
 
 const Home = () => {
-  const { filteredProducts } = useDataContext()
-
-  const [products, setProducts] = useState<IProductData[]>([])
   const limit = 18;
   const [skip, setSkip] = React.useState(0);
   const { data, isLoading, error } = useApi<IDataResponse>(
@@ -26,24 +22,15 @@ const Home = () => {
     setSkip((prev) => prev + limit);
   };
 
-  useEffect(() => {
-    if (filteredProducts.length) {
-      setProducts(filteredProducts)
-    }
-    else if (data) {
-      setProducts(data.products)
-    }
-  }, [data, filteredProducts])
-
   return (
-    <main className="m-20 mb-4 flex min-h-screen flex-col items-center justify-between">
+    <main className="m-20 flex min-h-screen flex-col items-center justify-between">
       <Container>
         {isLoading ? (
           <LoadingSpinner />
         ) : error ? (
           <p>Error: {error.message}</p>
         ) : (
-          <ProductsTemplate products={products} />
+          <ProductsTemplate products={data!.products} />
         )}
         <Pagination
           limit={limit}
